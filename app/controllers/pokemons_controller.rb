18ad71a03e49c783a91c39a80725e5394a83966f
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-30 21:46:59 -0400

    Done.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,16 @@
 class PokemonsController < ApplicationController
 
   def create
-    
+    trainer = Trainer.find(params[:trainer_id])
+    if trainer.pokemons.size < 6
+      new_pokemon = Pokemon.randomly_create_new(params[:trainer_id])
+      render json: new_pokemon
+    end
+  end
+
+  def destroy
+    pokemon = Pokemon.find(params[:id])
+    pokemon.destroy
   end
 
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,11 +1,22 @@
 class Pokemon < ApplicationRecord
   belongs_to :trainer
 
-  def generate_new_nickname
+  def self.generate_new_nickname
     Faker::Name.first_name
   end
 
-  def generate_new_species
+  def self.generate_new_species
     Faker::Games::Pokemon.name
+  end
+
+  def self.randomly_create_new(trainer_id)
+    new_pokemon = Pokemon.new(trainer_id: trainer_id)
+    new_pokemon.nickname = Pokemon.generate_new_nickname
+    new_pokemon.species = Pokemon.generate_new_species
+    if new_pokemon.save
+      new_pokemon
+    else
+      # raise an error
+    end
   end
 end
